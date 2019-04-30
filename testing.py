4a13d2dcbe27f8d4db81a6aa00e235c1c8ca571b
Jacek <48552433+CallMeMantis@users.noreply.github.com>
2019-04-30 18:23:18 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -3,6 +3,24 @@ from scipy import misc
 from matplotlib import pyplot as plt
 import math
 
+# FOR EXPERIMENTING
+# filename  = "top_right_square.jpg"
+# img = misc.imread(filename)
+# print(img.__class__)                  # wyswietli numpy.ndarray
+# print(img.shape)                      # rozmiar tablicy numpyego (512,512)
+# print(img.dtype)                      # dane w tablicy (uint8)
+# print(img[:])                        # otworz obrazek w postaci macierzy
+# print(img[:,0][20])                      # pierwszy wiersz
+# print(img[:,0][0][0])                   # pierwszy wyraz w tym wierszu
+# len(img)     ile wierszy
+# len(img[0])    ile kolumn
+# print(img[0][1])
+
+# COMMENTS
+# Try to make "TopLeft" part as simple as "TopRight"
+
+
+# ------------------------------------------------------------------------------------------
 # square matrix only
 class Computer_tomography:
 
@@ -31,10 +49,12 @@ class Computer_tomography:
 
         return rays_detected[:]
 
+
     # ray goes from top left corner
     def TopLeft(self):
         rays_detected = []
 
+        # rays_detected - from top right corner
         for col in range(len(self.img) - 1, -1, -1):
             index = 0
             for row in range((len(self.img)-1) - col):    # 10 - 1 - [9,8,7]
@@ -50,6 +70,7 @@ class Computer_tomography:
                 index += 1
         rays_detected.append(index)
 
+        # rays_detected - from top right corner
         for row in range(len(self.img) - 1, -1, -1):  # [9 8 7 ]
             index = 0
             for col in range((len(self.img)-1) - row):    # 10 - 1 - [9,8,7]     [0,1,2]
@@ -76,8 +97,8 @@ class Computer_tomography:
         for i in range(len(self.img)):
             list.append(i)
 
-        for i in range(len(self.img)):
-            tsil.append(list[-i])
+        for i in range(len(self.img) - 1,-1,-1):
+            tsil.append(i)
 
         rays_detected = []
         row = 0
@@ -88,13 +109,27 @@ class Computer_tomography:
                 if sum(self.img[row + j][col - j]) <= 50:
                     index += 1
             rays_detected.append(index)
-        return rays_detected
 
+        # after diagonal
+        col = len(list) - 1
+        # for row in range(1,len(list) - 1):      # (0-9)
+        #     print("NEXT ROW")
+        #     index = 0
+        #     for j in range(len(list) - 1):
+        #         print(row + j,col - j)
+
+        for j in range(len(list) - 1):
+            index = 0
+            print("NEXT ROW")
+            for row in range(1,len(list)):
+                print(row + j, col - j)
+
+        return rays_detected
 
 
 ct = Computer_tomography()
 memory = ct.TopRight()
-print(memory)
+# print(memory)
 
 
 
